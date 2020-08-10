b2382edd3fc610455b58a25cf4edfebc1270d31e
jakeee51 <jakemorfe@gmail.com>
2020-08-10 17:44:21 -0400

    0.1.3

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/jakeee51/gmapi",
+    install_requires = ["requests_html", "selenium", "bs4"],
     packages = setuptools.find_packages(),
     py_modules = ["gmapss", "busytimes"],
     classifiers = [
