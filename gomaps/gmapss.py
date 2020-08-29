e4a0fc467a65393af69674d1cb6cdcdc853d8f2a
jakeee51 <jakemorfe@gmail.com>
2020-08-29 00:30:33 -0400

    0.2.5

--- gomaps/gmapss.py
+++ gomaps/gmapss.py
@@ -5,7 +5,7 @@ Application Name: gmapss (Google Maps Search)
 Functionality Purpose: Acquire google maps data of a place based on query
 Version: Beta
 '''
-#8/26/20
+#8/29/20
 
 # &gws_rd=cr -> implement optional redirects
 
